dd9c49dd5fddd21dc8eae6b9284207a8128a9d79
ismailhiz <isomaohiz123@gmail.com>
2025-06-01 01:02:37 +0300

    saat bitti

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -1,5 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import '../assets/css/Clock.css'; 
+import '@fontsource/orbitron';
+import '@fontsource/share-tech-mono';
+import '@fontsource/rajdhani';
+import '@fontsource/space-mono';
+import '@fontsource/chakra-petch';
+import '@fontsource/audiowide';
+
 
 const Clock = () => {
   const [time, setTime] = useState(new Date());
@@ -25,12 +32,19 @@ const Clock = () => {
         {time.toLocaleTimeString()}
       </div>
       {!isFullScreen && (
-        <div className="theme-selector">
-          <button onClick={() => changeTheme('default')}>Varsayılan</button>
-          <button onClick={() => changeTheme('dark')}>Koyu Tema</button>
-          <button onClick={() => changeTheme('neon')}>Neon Tema</button>
-        </div>
-      )}
+  <div className="theme-selector">
+    <select onChange={(e) => changeTheme(e.target.value)} value={theme}>
+      <option value="default">Varsayılan</option>
+      <option value="dark">Koyu Tema</option>
+      <option value="neon">Neon Tema</option>
+      <option value="black-white">Rajdhani</option>
+      <option value="space-mono">Space Mono</option>
+      <option value="red-glow">Chakra Petch</option>
+      <option value="Audiowide">Audiowide</option>
+    </select>
+  </div>
+)}
+
     </div>
   );
 };
